7f870171c5e4450a33d94d40a9d25fa097dce315
HerGizHappa <gizem.erguen97@hotmail.com>
2023-07-24 15:06:12 +0200

    aktuellste version

--- script.js
+++ script.js
@@ -64,11 +64,11 @@ document.getElementById("drinkButton3").addEventListener("click", function() {
   var magic_bilder = document.getElementById("GinTonic_magic");
 
   // Überprüfen Sie das aktuelle Bild und ändern Sie die Bildquelle basierend darauf.
-  if (magic_bilder.src.endsWith("cocktail_ginTonic_magic.jpg")) {
-    magic_bilder.src = "newmagicGinTonic.jpg"; // Pfad zum neuen Bild, z. B. bild2.jpg
+  if (magic_bilder.src.endsWith("pic/cocktail_ginTonic_magic.jpg")) {
+    magic_bilder.src = "pic/newmagicGinTonic.jpg"; // Pfad zum neuen Bild, z. B. bild2.jpg
     magic_bilder.alt = "newmagic_GinTonic"; // Alternativer Text für das neue Bild
   } else {
-    magic_bilder.src = "cocktail_ginTonic_magic.jpg"; // Pfad zum ursprünglichen Bild
+    magic_bilder.src = "pic/cocktail_ginTonic_magic.jpg"; // Pfad zum ursprünglichen Bild
     magic_bilder.alt = "Gin Tonic-magic"; // Alternativer Text für das ursprüngliche Bild
   }
 });
