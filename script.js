03fce2adbe2f01c8438de6c5f02697c7615d6af1
HerGizHappa <gizem.erguen97@hotmail.com>
2023-07-31 04:56:18 +0200

    problem with API

--- script.js
+++ script.js
@@ -1,6 +1,96 @@
 // JavaScript für die Website
+/* Anfrage für den Drink pina colada */
+
+function fetchDataPinaColada() {
+  fetch('https://www.thecocktaildb.com/api/json/v1/1/search.php?s=pina colada')
+  .then(res => res.json())
+  .then(dataPinaColada => {
+      displayDataPinaColada(dataPinaColada.drinks);
+      console.log(dataPinaColada.drinks);
+  })
+  .catch(error => {
+      console.log('Abruf hat nicht funktioniert:', error)
+  })
+}
+
+function displayDataPinaColada(dataPinaColada) {
+  const drinkDiv = document.getElementById("drink-pinacolada");
+
+  const heading = document.createElement("h1");
+  heading.innerHTML = dataPinaColada[0].strDrink;
+  drinkDiv.appendChild(heading);
+
+  const description = document.createElement("p");
+  description.innerHTML = dataPinaColada[0].strInstructions;
+  drinkDiv.appendChild(description);
+
+}
+
+/* Anfrage für den Drink mojito */
+
+function fetchDataMojito() {
+  fetch('https://www.thecocktaildb.com/api/json/v1/1/search.php?s=mojito')
+  .then(res => res.json())
+  .then(dataMojito => {
+      displayDataMojito(dataMojito.drinks);
+      console.log(dataMojito.drinks);
+  })
+  .catch(error => {
+      console.log('Abruf hat nicht funktioniert:', error)
+  })
+}
+
+function displayDataMojito(dataMojito) {
+  const drinkDiv = document.getElementById("drink-mojito");
+
+  const heading = document.createElement("h1");
+  heading.innerHTML = dataMojito[0].strDrink;
+  drinkDiv.appendChild(heading);
+
+  const description = document.createElement("p");
+  description.innerHTML = dataMojito[0].strInstructions;
+  drinkDiv.appendChild(description);
+
+}
+
+/* Anfrage für den Drink gin tonic */
+
+function fetchDataGinTonic() {
+  fetch('https://www.thecocktaildb.com/api/json/v1/1/search.php?s=gin tonic')
+  .then(res => res.json())
+  .then(dataGinTonic => {
+      displayDataGinTonic(dataGinTonic.drinks);
+      console.log(dataGinTonic.drinks);
+  })
+  .catch(error => {
+      console.log('Abruf hat nicht funktioniert:', error)
+  })
+}
+
+function displayDataGinTonic(dataGinTonic) {
+  const drinkDiv = document.getElementById("drink-gintonic");
+
+  const heading = document.createElement("h1");
+  heading.innerHTML = dataGinTonic[0].strDrink;
+  drinkDiv.appendChild(heading);
+
+  const description = document.createElement("p");
+  description.innerHTML = dataGinTonic[0].strInstructions;
+  drinkDiv.appendChild(description);
+
+}
+
+
+/* allgemeiner JavaScript Aufruf für alle Events */
+
+document.addEventListener("DOMContentLoaded", function() {
+  fetchData();
+  fetchDataPinaColada();
+  fetchDataMojito();
+  fetchDataGinTonic();
+});
 // Funktion, um das Dropdown-Menü ein- und auszublenden
-function toggleMenu() {
+/* function toggleMenu() {
   var dropdownContent = document.getElementById("dropdown");
   dropdownContent.style.display = dropdownContent.style.display === "block" ? "none" : "block";
 }
@@ -58,5 +148,5 @@ document.getElementById("drinkButton3").addEventListener("click", function() {
     magic_bilder.src = "pic/cocktail_ginTonic_magic.jpg"; // Pfad zum ursprünglichen Bild
     magic_bilder.alt = "Gin Tonic-magic"; // Alternativer Text für das ursprüngliche Bild
   }
-});
+}); */
 
