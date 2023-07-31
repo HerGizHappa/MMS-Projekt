06b5f7d0d1bf9bc90f930be43d8343b8927b14a7
HerGizHappa <gizem.erguen97@hotmail.com>
2023-07-31 22:58:33 +0200

    lalaaaa

--- script.js
+++ script.js
@@ -99,7 +99,7 @@ document.getElementById("drinkButton1").addEventListener("click", function() {
   var magic_bilder = document.getElementById("PinaColada_magic");
 
   if (magic_bilder.src.endsWith("cocktail_pinaColada_magic.JPG")) {
-    magic_bilder.src = "newmagicpina.jpg"; // Pfad zum neuen Bild, z. B. bild2.jpg
+    magic_bilder.src = "newmagicpina.JPG"; // Pfad zum neuen Bild, z. B. bild2.jpg
     magic_bilder.alt = "newmagic_pina"; // Alternativer Text für das neue Bild
   } else {
     magic_bilder.src = "cocktail_pinaColada_magic.JPG"; // Pfad zum ursprünglichen Bild
@@ -113,7 +113,7 @@ document.getElementById("drinkButton2").addEventListener("click", function() {
   var magic_bilder = document.getElementById("Mojito_magic");
 
   if (magic_bilder.src.endsWith("cocktail_Mojito_magic.JPG")) {
-    magic_bilder.src = "newmagicmojito.jpg"; // Pfad zum neuen Bild, z. B. bild2.jpg
+    magic_bilder.src = "newmagicmojito.JPG"; // Pfad zum neuen Bild, z. B. bild2.jpg
     magic_bilder.alt = "newmagic_mojito"; // Alternativer Text für das neue Bild
   } else {
     magic_bilder.src = "cocktail_Mojito_magic.JPG"; // Pfad zum ursprünglichen Bild
@@ -127,7 +127,7 @@ document.getElementById("drinkButton3").addEventListener("click", function() {
   var magic_bilder = document.getElementById("GinTonic_magic");
 
   if (magic_bilder.src.endsWith("pic/cocktail_ginTonic_magic.JPG")) {
-    magic_bilder.src = "pic/newmagicGinTonic.jpg"; // Pfad zum neuen Bild, z. B. bild2.jpg
+    magic_bilder.src = "pic/newmagicGinTonic.JPG"; // Pfad zum neuen Bild, z. B. bild2.jpg
     magic_bilder.alt = "newmagic_GinTonic"; // Alternativer Text für das neue Bild
   } else {
     magic_bilder.src = "pic/cocktail_ginTonic_magic.JPG"; // Pfad zum ursprünglichen Bild
