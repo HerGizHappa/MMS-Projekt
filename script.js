19778cf545b81be5660e4bc6682633db129500be
HerGizHappa <gizem.erguen97@hotmail.com>
2023-07-31 22:31:20 +0200

    neu bild

--- script.js
+++ script.js
@@ -53,11 +53,11 @@ document.getElementById("drinkButton1").addEventListener("click", function() {
   // Holt sich Bild-Element durch seine ID.
   var magic_bilder = document.getElementById("PinaColada_magic");
 
-  if (magic_bilder.src.endsWith("cocktail_pinaColada_magic.jpg")) {
+  if (magic_bilder.src.endsWith("cocktail_pinaColada_magic.JPG")) {
     magic_bilder.src = "newmagicpina.jpg"; // Pfad zum neuen Bild, z. B. bild2.jpg
     magic_bilder.alt = "newmagic_pina"; // Alternativer Text für das neue Bild
   } else {
-    magic_bilder.src = "cocktail_pinaColada_magic.jpg"; // Pfad zum ursprünglichen Bild
+    magic_bilder.src = "cocktail_pinaColada_magic.JPG"; // Pfad zum ursprünglichen Bild
     magic_bilder.alt = "Pina Colada-magic"; // Alternativer Text für das ursprüngliche Bild
   }
 });
@@ -67,11 +67,11 @@ document.getElementById("drinkButton2").addEventListener("click", function() {
   // Holt sich Bild-Element durch seine ID.
   var magic_bilder = document.getElementById("Mojito_magic");
 
-  if (magic_bilder.src.endsWith("cocktail_Mojito_magic.jpg")) {
+  if (magic_bilder.src.endsWith("cocktail_Mojito_magic.JPG")) {
     magic_bilder.src = "newmagicmojito.jpg"; // Pfad zum neuen Bild, z. B. bild2.jpg
     magic_bilder.alt = "newmagic_mojito"; // Alternativer Text für das neue Bild
   } else {
-    magic_bilder.src = "cocktail_Mojito_magic.jpg"; // Pfad zum ursprünglichen Bild
+    magic_bilder.src = "cocktail_Mojito_magic.JPG"; // Pfad zum ursprünglichen Bild
     magic_bilder.alt = "Mojito-magic"; // Alternativer Text für das ursprüngliche Bild
   }
 });
@@ -81,11 +81,11 @@ document.getElementById("drinkButton3").addEventListener("click", function() {
   // Holt sich Bild-Element durch seine ID.
   var magic_bilder = document.getElementById("GinTonic_magic");
 
-  if (magic_bilder.src.endsWith("pic/cocktail_ginTonic_magic.jpg")) {
+  if (magic_bilder.src.endsWith("pic/cocktail_ginTonic_magic.JPG")) {
     magic_bilder.src = "pic/newmagicGinTonic.jpg"; // Pfad zum neuen Bild, z. B. bild2.jpg
     magic_bilder.alt = "newmagic_GinTonic"; // Alternativer Text für das neue Bild
   } else {
-    magic_bilder.src = "pic/cocktail_ginTonic_magic.jpg"; // Pfad zum ursprünglichen Bild
+    magic_bilder.src = "pic/cocktail_ginTonic_magic.JPG"; // Pfad zum ursprünglichen Bild
     magic_bilder.alt = "Gin Tonic-magic"; // Alternativer Text für das ursprüngliche Bild
   }
 }); 
